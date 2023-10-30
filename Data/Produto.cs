cec67bb14b9da4b2681dea874f005d6239499258
pduartp <patrick.d.pimenta@gmail.com>
2023-10-30 08:39:34 -0300

    Initial Commit

---
+++ Data/Produto.cs
@@ -0,0 +1,9 @@
+namespace info_app.Data;
+
+public class Produto{
+    public int Id { get; set; }
+    public string? Nome { get; set; }
+    public double Preço { get; set; }
+    public string? Descr { get; set; }
+    public int Quant { get; set; }
+}
